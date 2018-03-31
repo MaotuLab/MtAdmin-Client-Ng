47ee39a63f32f7562e73d0d5d05d0a1ce2f8ff75
iallai <iallai.info@gmail.com>
2018-03-31 23:12:43 +0800

    modify layout

--- src/app/layout/base.layout.component.ts
+++ src/app/layout/base.layout.component.ts
@@ -7,25 +7,5 @@ import { Component } from '@angular/core';
 })
 export class BaseLayout {
   isCollapsed: boolean = true;
-  page :Number= 1;
-  isLoading :boolean= false;
-  projects  =[
-    {id:1,title:'Mt Cola',imageUrl:'/assets/images/test/image5.jpg'},
-    {id:1,title:'Mt Admin',imageUrl:'/assets/images/test/image1.jpg'},
-    {id:1,title:'Mt Shop',imageUrl:'/assets/images/test/image7.jpg'},
-    {id:1,title:'Mt Api',imageUrl:'/assets/images/test/image3.jpg'},
-    {id:1,title:'Mt Share',imageUrl:'/assets/images/test/image4.jpg'},
-
-    {id:1,title:'Mt Sales',imageUrl:'/assets/images/test/image6.jpg'},
-
-  ]
-  public ngOnInit():void{ 
-    this.isLoading = true; 
-    setTimeout(()=>{
-      this.projects.map(item=>{
-        item.imageUrl=`url(${item.imageUrl})` 
-      })
-      this.isLoading =false;
-    },1000)
-  }
+ 
 }
